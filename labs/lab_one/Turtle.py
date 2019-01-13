d79b45e01c1125c6af90a909688cdd6d9d4fb590
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-01-13 13:37:23 +0300

    Start turtle exercises

---
+++ labs/lab_one/Turtle.py
@@ -0,0 +1,29 @@
+import turtle
+
+# turtle.shape('turtle')
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(50)
+
+
+# square turtle
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+# circle turtle
+
+
